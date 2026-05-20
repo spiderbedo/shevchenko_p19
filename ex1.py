80603dfa04b0d08eb3b241e04bc3dbd25c1e9735
spiderbedo <134661658+spiderbedo@users.noreply.github.com>
2026-05-21 03:08:36 +0700

    Update ex1.py

--- ex1.py
+++ ex1.py
@@ -1,1 +1,30 @@
+class Dog:
+    """
+    A class representing a dog.
+    """
 
+    def __init__(self, name: str):
+        """
+        Initialize a dog with a given name.
+        """
+
+        self.name = name
+
+
+    def say(self) -> None:
+        """
+        Print the barking sound of the dog.
+        """
+
+        print("Гав!")
+
+
+    def __str__(self) -> str:
+        """
+        Return the string representation of the dog.
+        """
+
+        return self.name
+
+
+    __repr__ = __str__
